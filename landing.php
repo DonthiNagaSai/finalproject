c25979afa65d3b40057f2f78beef1405ef415c1e
DonthiNagaSai <nagasaidonthii@gmail.com>
2024-08-09 18:08:37 +0530

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+
+ 	<head>
+ 		<!-- Meta Tags -->
+		<meta charset="UTF-8">
+		<meta name="author" content="Kamran Mubarik">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Title -->
+ 		<title>Maithreya Agencies</title>
+ 		<!-- Style Sheet -->
+		<link rel="stylesheet" type="text/css" href="Homepage1/css/style.css" />
+		<!-- Javascript -->	
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+  		<script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+  		<script>
+		    $(document).ready(function(){
+		      $('.slider').bxSlider({
+				  auto: true,
+				  autoControls: true,
+				  stopAutoOnClick: true,
+				  pager: true
+				});
+		    });
+		 </script>
+ 	</head>
+<body>
+
+	<header>
+		<div class="container">
+			<div class="brand">
+				<div class="logo">
+					<a href="login_form.php">
+						<img src="Homepage1/img/icons/online_shopping.png">
+						<div class="logo-text">
+							<p class="big-logo">Maithreya</p>
+							<p class="small-logo">Agencies</p>
+						</div>
+					</a>
+				</div> <!-- logo -->
+				<div class="shop-icon">
+					
+					
+					<div class="dropdown">
+					<a href="login_form.php"><img src="Homepage1/img/icons/download.png" ></a>
+					</div>
+				</div> <!-- shop icons -->
+			</div> <!-- brand -->
+
+			<div class="menu-bar">
+				<div class="menu">
+					<ul>
+						<li><a href="login_form.php">Home</a></li>
+						<li><a href="login_form.php">Shop</a></li>
+						<li><a href="login_form.php">About</a></li>
+						<li><a href="login_form.php">Contact</a></li>
+					</ul>
+				</div>
+				<div class="search-bar">
+					<form>
+						<div class="form-group">
+							<input type="text" class="form-control" name="search" placeholder="Search">
+							<img src="Homepage1/img/icons/search.png">
+						</div>
+					</form>
+				</div>
+			</div> <!-- menu -->
+		</div> <!-- container -->
+	</header> <!-- header -->
+
+	<div class="container">
+		<main>
+		<div class="slider">
+				<div class="slide-1">
+				<a href="login_form.php"><img src="Homepage1/img/slider/slide-1.jpg"></a>
+					<div class="slider-text">
+						<h3>Sales On</h3>
+						<h2>ENGINE OILS</h2>
+						
+					</div>
+				</div>
+					
+			</div> <!-- slider -->
+
+			<div class="new-product-section">
+				<div class="product-section-heading">
+					<h2>Related products<img src="Homepage1/img/icons/increase.png"></h2>
+					<h3>Most selling product</h3>
+				</div>
+				<div class="product-content">
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img1.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>Tyres/Brands</h3>
+							<h2>Brand type : MRF TYRES</h2>
+						</div>						
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img2.jpg">
+						</a>
+						<div class="product-detail">
+						    <h3>Tyres/Brands</h3>
+							<h2>Brand type :CEAT TYRES</h2>
+						</div>
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img3.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>Tyres/Brands</h3>
+							<h2>Brand type :APPOLO TYRES</h2>
+						</div>
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img4.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>Tyres/Brands</h3>
+							<h2>Brand type :JK TYRES</h2>
+						</div>
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img5.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>ENGINE OILS</h3>
+							<h2>SERVO 15W-40</h2>
+						</div>
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img6.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>ENGINE OILS</h3>
+							<h2>SHELL 15W-50</h2>
+						</div>
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img7.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>ENGINE OILS</h3>
+							<h2>MOBIL 15W-40</h2>
+						</div>
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img8.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>ENGINE OILS</h3>
+							<h2>CASTROL POWER 1 20W-50</h2>
+						</div>
+					</div>
+				</div>
+			</div> <!-- New Product Section -->
+
+			<div class="collection">
+				<div class="men-collection">
+					<a href="login_form.php"><h2>ENGINE OILS</h2></a>
+				</div>
+				<div class="women-collection">
+					<a href="login_form.php"><h2>TYRES</h2></a>
+				</div>
+			</div> <!-- Collection Section -->
+
+			<div class="new-product-section">
+				<div class="product-section-heading">
+					<h2>Recommend Products <img src="Homepage1/img/icons/good_quality.png"></h2>
+					<h3>OUR BEST PRODUCTS RECOMMENDED FOR YOU</h3>
+				</div>
+				<div class="product-content">
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img1.jpg">
+						</a>
+						<div class="product-detail">
+						<h3>Tyres/Brands</h3>
+							<h2>Brand type : MRF TYRES</h2>
+						</div>						
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img2.jpg">
+						</a>
+						<div class="product-detail">
+						    <h3>Tyres/Brands</h3>
+							<h2>Brand type :CEAT TYRES</h2>
+						</div>
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img7.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>ENGINE OILS</h3>
+							<h2>MOBIL 15W-40</h2>
+						</div>
+					</div>
+					<div class="product">
+						<a href="login_form.php">
+							<img src="Homepage1/img/product/img8.jpg">
+						</a>
+						<div class="product-detail">
+							<h3>ENGINE OILS</h3>
+							<h2>CASTROL POWER 1 20W-50</h2>
+						</div>
+					</div>
+				</div>
+			</div> <!-- Recommend Product Section -->
+		</main> <!-- Main Area -->
+	</div>
+
+	<footer>
+		<div class="container">
+			<div class="footer-widget">
+				<div class="widget">
+					<div class="widget-heading">
+						<h3>Important Link</h3>
+					</div>
+					<div class="widget-content">
+						<ul>
+							<li><a href="login_form.php">About</a></li>
+							<li><a href="login_form.php">Contact</a></li>
+							<li><a href="login_form.php">Terms & Conditions</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="widget">
+					<div class="widget-heading">
+						<h3>Information</h3>
+					</div>
+					<div class="widget-content">
+						<ul>
+							<li><a href="login_form.php">My Account</a></li>
+							<li><a href="login_form.php">My Orders</a></li>
+							<li><a href="login_form.php">Cart</a></li>
+							<li><a href="login_form.php">Checkout</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="widget">
+					<div class="widget-heading">
+						<h3>Follow us</h3>
+					</div>
+					<div class="widget-content">
+						<div class="follow">
+							<ul>
+								<li><a href="#"><img src="Homepage1/img/icons/facebook.png"></a></li>
+								<li><a href="#"><img src="Homepage1/img/icons/twitter.png"></a></li>
+								<li><a href="#"><img src="Homepage1/img/icons/instagram.png"></a></li>
+							</ul>
+						</div>						
+					</div>
+					<div class="widget-heading">
+						<h3>Subscribe for Newsletter</h3>
+					</div>
+					<div class="widget-content">
+						<div class="subscribe">
+							<form>
+								<div class="form-group">
+									<input type="text" class="form-control" name="subscribe" placeholder="Email">
+									<img src="Homepage1/img/icons/paper_plane.png">
+								</div>
+							</form>
+						</div>						
+					</div>
+				</div>
+			</div> <!-- Footer Widget -->
+		</div>
+	</footer> <!-- Footer Area -->
+
+</body>
+
+        </html>
